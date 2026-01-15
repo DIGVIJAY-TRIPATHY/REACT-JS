4fe38a0ee9c11cb049f2cc4a50c6b53f5f1baf30
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-01-15 20:06:54 +0530

    learning about portal

---
+++ FULL-REACT/04-portal/src/components/CopyInput.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+
+const CopyInput = () => {
+
+  const [inputValue, setInputValue] = useState('')
+  const [copied, setCopied] = useState(false)
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default CopyInput
